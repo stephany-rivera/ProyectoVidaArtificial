755159f82d044f398527a7bf4fbf4c025c3164a0
Santiago-Romero <38481577+Santiago-Romero@users.noreply.github.com>
2018-05-21 18:01:42 -0500

    Propongo
    Propongo manejar los 3 tipos de agentes de esta manera pero agregando los atributos que subió Michael, No se solo digo, igual se podria tratar el movimiento desde otra clase con un hilo como se muestra comentado en la clase "MostrarSimulacion".      :v. Igualmente el movimiento conectado aun no funciona bien supongo que debemos agregar otros factores aparte de solo un movimiento aleatorio

---
+++ ProyectoVida/src/Logica/Lobo.java
@@ -0,0 +1,44 @@
+package Logica;
+
+/**
+ *
+ * @author Santiago Romero
+ */
+public class Lobo {
+    int id;
+    int x;
+    int y;
+    
+    public Lobo(int id, int x, int y){
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    
+}
+
---
+++ ProyectoVida/src/Logica/Pasto.java
@@ -0,0 +1,42 @@
+package Logica;
+
+/**
+ *
+ * @author Santiago Romero
+ */
+public class Pasto {
+    int id;
+    int x;
+    int y;
+    
+    public Pasto(int id, int x, int y){
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+}
